0d55236137885694f62054d9d1757e8b433058d0
EZ_Ezral <ezrxlezni@gmail.com>
2026-03-19 15:53:06 +0800

    Update system.py

--- system.py
+++ system.py
@@ -1,19 +1,16 @@
 def detect_board():
-    
     # --- Try MicroPython board detection ---
     try:
         import machine
-        
         board = machine.uname().machine
         
         if "RP2" in board or "rp2" in board or "RP2040" in board:
             return "RP2035 / RP2040 Board"
         
         return "MicroPython Board (Unknown)"
-    
+        
     except:
         pass
-
     # --- Try Raspberry Pi detection ---
     try:
         with open("/proc/cpuinfo", "r") as f:
@@ -27,11 +24,10 @@ def detect_board():
     
     except:
         pass
-
+        
     # --- Fallback ---
     import platform
     return f"Unknown Platform ({platform.system()})"
-
-
+    
 # Test
 print(detect_board())
