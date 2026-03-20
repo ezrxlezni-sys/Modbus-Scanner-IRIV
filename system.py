a7522e801bb4852ad611885e9231e4a160e54093
EZ_Ezral <ezrxlezni@gmail.com>
2026-03-20 16:11:10 +0800

    Update system.py

--- system.py
+++ system.py
@@ -1,10 +1,10 @@
 def detect_platform():
-    # --- Check Microcontroller (MicroPython) ---
-    print("Checking board using MicroPython method...")
+    # --- Check CircuitPython board ---
+    print("Checking board using CircuitPython method...")
     try:
         import board
-        return "Microcontroller"
-    except:
+        return "IOC"
+    except Exception:
         pass
 
     # --- Check Raspberry Pi (Linux) ---
@@ -12,8 +12,8 @@ def detect_platform():
     try:
         with open("/proc/cpuinfo", "r") as f:
             if "Raspberry Pi" in f.read():
-                return "Raspberry Pi"
-    except:
+                return "PiControl"
+    except Exception:
         pass
 
     return "Unknown"
@@ -23,13 +23,13 @@ def main():
     platform = detect_platform()
     print("Detected Platform:", platform)
 
-    if platform == "Microcontroller":
-        import ioc
-        ioc.scan_modbus()
+    if platform == "IOC":
+        from ioc import scan_modbus
+        scan_modbus()
 
-    elif platform == "Raspberry Pi":
-        import picontrol
-        picontrol.scan_modbus()
+    elif platform == "PiControl":
+        from picontrol import scan_modbus
+        scan_modbus()
 
     else:
         print("Unsupported platform")
